1f4cbe48286c2c5378314394cf997265b32ac2d6
Ori Nakel <nakelori@gmail.com>
2026-06-01 10:40:31 +0300

    Add colordetc.py and limits.py

---
+++ colordetc.py
@@ -0,0 +1,23 @@
+import cv2
+from PIL import Image
+from limits import get_limits
+
+yellow = [0,255,255]
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    hsvimg = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    low , up = get_limits(color = yellow)
+    mask = cv2.inRange(hsvimg,low,up)
+    mask1 = Image.fromarray(mask)
+    box = mask1.getbbox()
+    if box is not None:
+        x1,y1,x2,y2 = box
+        frame = cv2.rectangle(frame, (x1,y1),(x2,y2),(255,255,0),2)
+    
+    cv2.imshow('cam', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
