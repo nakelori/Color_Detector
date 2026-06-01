2146d8e7597764f4673dbe2b7ee6c4b90c01d9ed
Ori Nakel <nakelori@gmail.com>
2026-06-01 10:44:13 +0300

    Update colordetc.py logic

--- colordetc.py
+++ colordetc.py
@@ -7,14 +7,14 @@ cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
-    hsvimg = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    low , up = get_limits(color = yellow)
-    mask = cv2.inRange(hsvimg,low,up)
-    mask1 = Image.fromarray(mask)
-    box = mask1.getbbox()
+    hsvimg = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) #HSV convert
+    low , up = get_limits(color = yellow) 
+    mask = cv2.inRange(hsvimg,low,up) #image masking by color limits
+    mask1 = Image.fromarray(mask) 
+    box = mask1.getbbox() #bounding box
     if box is not None:
         x1,y1,x2,y2 = box
-        frame = cv2.rectangle(frame, (x1,y1),(x2,y2),(255,255,0),2)
+        frame = cv2.rectangle(frame, (x1,y1),(x2,y2),(255,255,0),2) 
     
     cv2.imshow('cam', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
